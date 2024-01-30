cd0da045a4fccdf01cd607990d4dc00f5e0260c1
Dave Williams <davejjwilliams@gmail.com>
2024-01-30 15:04:22 +0100

    Cleaned Up Carousel Code

--- src/components/subcomponents/ProjectCarousel.js
+++ src/components/subcomponents/ProjectCarousel.js
@@ -4,83 +4,27 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import Project from './Project';
 
-// const responsiveProps = {
-//   0: {
-//     items: 1
-//   },
-//   1024: {
-//     items: 2,
-//     itemsFit: 'contain'
-//   },
-//   1600: {
-//     items: 3,
-//     itemsFit: 'contain'
-//   }
-// };
-
-const projects = [
-  { title: 'B-Assist', desc: 'Description' },
-  { title: 'Digital FGD Platform', desc: 'Description' },
-  {
-    title: 'Manutrackture',
-    desc: 'Description'
-  },
-  { title: 'Project 4', desc: 'Description' },
-  { title: 'Project 5', desc: 'Description' }
+const items = [
+  <Project key={1} title={'B-Assist'} desc={'Description'} />,
+  <Project key={2} title={'Digital FGD Platform'} desc={'Description'} />,
+  <Project key={3} title={'Manutrackture'} desc={'Description'} />
 ];
-
-const createItems = (items, [handleClick]) => {
-  let deltaX = 0;
-  let difference = 0;
-  const swipeDelta = 20;
-
-  return items.map((item, i) => (
-    <div
-      data-value={i + 1}
-      className='item'
-      onMouseDown={e => (deltaX = e.pageX)}
-      onMouseUp={e => (difference = Math.abs(e.pageX - deltaX))}
-      onClick={() => difference < swipeDelta && handleClick(i)}
-    >
-      <Project title={item.title} desc={item.desc} />
-    </div>
-  ));
-};
 
 const ProjectCarousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [items] = useState(createItems(projects, [setActiveIndex]));
-
   const slidePrev = () => {
     if (activeIndex === 0) {
-      setActiveIndex(projects.length - 1);
+      setActiveIndex(items.length - 1);
     } else {
       setActiveIndex(activeIndex - 1);
     }
   };
   const slideNext = () => {
-    if (activeIndex === projects.length - 1) {
+    if (activeIndex === items.length - 1) {
       setActiveIndex(0);
     } else {
       setActiveIndex(activeIndex + 1);
     }
-  };
-
-  const syncActiveIndexForSwipeGestures = e => setActiveIndex(e.item);
-
-  const onSlideChanged = e => {
-    syncActiveIndexForSwipeGestures(e);
-    console.debug(
-      `onSlideChanged => Item's position after changes: ${e.item}. Event:`,
-      e
-    );
-  };
-
-  const onUpdated = e => {
-    console.debug(
-      `onUpdated => Item's position after update: ${e.item}. Event:`,
-      e
-    );
   };
 
   return [
@@ -90,9 +34,6 @@ const ProjectCarousel = () => {
       disableButtonsControls
       items={items}
       activeIndex={activeIndex}
-      // responsive={responsiveProps}
-      onSlideChanged={onSlideChanged}
-      onUpdated={onUpdated}
     />,
     <div className='b-refs-buttons project-nav-buttons'>
       <button onClick={slidePrev} className='intro-button'>
