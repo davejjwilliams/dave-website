0a54c343cf2575f52db0716b6df175403db487d0
Dave Williams <davejjwilliams@gmail.com>
2024-01-30 15:58:09 +0100

    Publication Link Opens in New Tab

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -2,12 +2,18 @@ const Publications = () => {
   return (
     <div>
       <div className='title'>Publications</div>
+      <div className='subtitle'>2023</div>
       D. Williams, J. Callan, S. Kirbas, S. Mechtaev, J. Petke, T. Prideaux-Ghee
       and Federica Sarro, “User-Centric Deployment of Automated Program Repair
       at Bloomberg,” 2023,{' '}
-      <a className='highlight' href='https://arxiv.org/abs/2311.10516'>
+      <a
+        className='highlight'
+        href='https://arxiv.org/abs/2311.10516'
+        target='_blank'
+      >
         arXiv:2311.10516
-      </a>
+      </a>{' '}
+      (Accepted at ICSE 2024 SEIP Track)
     </div>
   );
 };
